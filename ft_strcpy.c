d89c8b8a327fa76e3440db021e41473498e6f499
kntshoko <50703727+kntshoko@users.noreply.github.com>
2019-05-20 07:57:56 +0200

    Create ft_strcpy.c

---
+++ ft_strcpy.c
@@ -0,0 +1,12 @@
+char    *ft_strcpy(char *str1, char *str2)
+{
+    int cont;
+
+    cont = 0;
+    while(str2[cont])
+    {
+        str1[cont] = str2[cont];
+        cont++;
+    }
+    return(str1);
+}
