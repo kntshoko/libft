832acfa1149308e5bff933069136206582c0786d
kntshoko <50703727+kntshoko@users.noreply.github.com>
2019-05-20 15:12:01 +0200

    Update ft_strcmp.c

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -9,8 +9,5 @@ int ft_strlen(char *str)
 }
 int ft_strcmp(char *str1, char *str2)
 {
-    int cont;
-
-    cont = ft_strlen(str2) - ft_strlen(str1);
-    return (cont);
+    return (ft_strlen(str2) - ft_strlen(str1));
 }
