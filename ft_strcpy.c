acd9410dbbde1ca797296ec8fb2fd8fc955e8600
kntshoko <50703727+kntshoko@users.noreply.github.com>
2019-05-20 08:03:40 +0200

    ft_strcpy

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -3,10 +3,10 @@ char    *ft_strcpy(char *str1, char *str2)
     int cont;
 
     cont = 0;
-    while(str2[cont])
+    while (str2[cont])
     {
         str1[cont] = str2[cont];
         cont++;
     }
-    return(str1);
+    return (str1);
 }
