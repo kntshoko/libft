3d35410b2ab55cd2fd4008d30e01a486e60ab047
Khotso NTSHOKO <kntshoko@c4r14s3.wethinkcode.co.za>
2019-05-20 09:58:36 +0200

    ft_memcpy, ft_memset

---
+++ ft_memcpy.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kntshoko <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/20 09:40:37 by kntshoko          #+#    #+#             */
+/*   Updated: 2019/05/20 09:57:33 by kntshoko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+void	*memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	int len;
+
+	len = 0;
+	while (len ++ <= n)
+		dst[len] = src[len];
+	return (dst);
+}
+
