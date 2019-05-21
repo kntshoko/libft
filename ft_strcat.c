ec6df172ebcbf6b790a87d22b46262da68900bd2
Khotso NTSHOKO <kntshoko@c5r8s3.wethinkcode.co.za>
2019-05-21 16:10:36 +0200

    modified:   ft_strcat.c

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,10 +6,19 @@
 /*   By: kntshoko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 08:38:03 by kntshoko          #+#    #+#             */
-/*   Updated: 2019/05/21 08:38:07 by kntshoko         ###   ########.fr       */
+/*   Updated: 2019/05/21 16:09:52 by kntshoko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int ft_strlen(char *str)
+{
+    int cont;
+
+    cont = 0;
+    while (str[cont] != '\0')
+        cont++;
+    return(cont);
+}
 char    *ft_strcat(char *str1, char *str2)
 {
     int cont;
@@ -17,11 +26,11 @@ char    *ft_strcat(char *str1, char *str2)
 
     len = ft_strlen(str1);
     cont = 0;
-    while (str2[cont])
+    while (str2[cont] != '\0')
     {
-        str1[len] = str2[cont];
-        len ++;
+        str1[len + cont] = str2[cont];
         cont++;
     }
+	str1[len + cont] = '\0'; 
     return(str1);
 }
