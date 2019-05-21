7935a9cf089febca2cfd68a4fae7d87a7b3f9cf4
Khotso NTSHOKO <kntshoko@c5r8s3.wethinkcode.co.za>
2019-05-21 10:43:17 +0200

    modified:   ft_strcmp.c

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,20 +6,16 @@
 /*   By: kntshoko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 08:42:40 by kntshoko          #+#    #+#             */
-/*   Updated: 2019/05/21 08:42:59 by kntshoko         ###   ########.fr       */
+/*   Updated: 2019/05/21 10:42:16 by kntshoko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_strlen(char *str)
-{
-	int count;
-
-	count = 0;
-	while (str[count] != '\0')
-		count++;
-	return (count);
-}
 int ft_strcmp(char *str1, char *str2)
 {
-    return (ft_strlen(str2) - ft_strlen(str1));
+	int c;
+
+	 c = 0;
+	 while (str1[c] == str2[c] && str1[c] != '\0')
+			c++;
+	return(str1[c] - str2[c]);
 }
