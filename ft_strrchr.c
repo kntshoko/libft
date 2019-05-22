20b0b11711ed4d3838b77e5eb98ba437f0739034
Khotso NTSHOKO <kntshoko@c5r8s3.wethinkcode.co.za>
2019-05-22 11:40:33 +0200

    modified:   ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: kntshoko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 08:47:34 by kntshoko          #+#    #+#             */
-/*   Updated: 2019/05/21 18:08:36 by kntshoko         ###   ########.fr       */
+/*   Updated: 2019/05/22 11:39:37 by kntshoko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ char    *ft_strrchr(const char *str, int c)
 {
 	int len;
 
-	len = strlen(str); 
-    if (str [ len] == c)
-        return(char *)str;
-    while (len != -2)
-    {
-		len --;
-		if (len == -1)
-			return(NULL);
-   	}
-
-	return(char *)str;
+	len = strlen(str);
+    if ('\0' == (char)c)
+        return(strchr(str,'\0'));
+	len--;
+	while (len >= 0)
+	{
+		if(str[len] == (char)c)
+			return ((char *)&str[len]);
+		len--;
+	}
+	return(NULL);
 }
