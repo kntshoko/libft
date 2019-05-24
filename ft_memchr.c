147f00f26fc8360bf8566f7b24b09266a0b4705e
Khotso NTSHOKO <kntshoko@c5r8s3.wethinkcode.co.za>
2019-05-24 22:26:00 +0200

    modified:   ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,22 +6,24 @@
 /*   By: kntshoko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 10:21:56 by kntshoko          #+#    #+#             */
-/*   Updated: 2019/05/21 08:36:56 by kntshoko         ###   ########.fr       */
+/*   Updated: 2019/05/24 22:25:19 by kntshoko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-void	*ft_memchar(const void *s, int c, size_t n)
+void	*ft_memchr(const void *dst, int c , size_t n)
 {
-	int len;
+	const char *d = (const void *)dst;
+	size_t len;
 
-	while(len <= n)
+	len = 0;
+	while (len <= n)
 	{
-		if (*s == c)
-			return(char*)s;
-		len ++;
-		s ++;
+		if ( d[len] == (char)c)
+			return ((void *)&d[len]);
+		d++;
+		len++;
 	}
-	return (NULL);
+	return(NULL);
 }
