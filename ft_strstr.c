2e24cdd43c1017a1bd844334607e5d8a46c56574
Khotso NTSHOKO <kntshoko@c5r8s3.wethinkcode.co.za>
2019-05-25 03:32:19 +0200

    ft_strstr.c

---
+++ ft_strstr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kntshoko <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/25 03:29:19 by kntshoko          #+#    #+#             */
+/*   Updated: 2019/05/25 03:31:47 by kntshoko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int in;
+	int ih;
+	char *hy = (char *)haystack;
+	char *tocmp;
+
+	in = 0;
+	ih = 0;
+
+	if (strlen(needle) == 0)
+		return((char *)&haystack[ih]);
+	while (hy[ih] != '\0')
+	{
+		while (in < strlen(needle))
+		{
+			tocmp[in] = hy[ih + in];
+			in++;
+		}
+		if (strcmp(tocmp,needle) == 0)
+			return((char *)&haystack[ih]);
+		in = 0;
+		ih++;
+	}
+	return(NULL);
+}
