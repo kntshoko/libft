fa1e27be7d8fe61f9962190aa35407897f9e5d12
Khotso NTSHOKO <kntshoko@c5r8s3.wethinkcode.co.za>
2019-05-27 10:22:12 +0200

    modified:   ft_memmove.cw

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: kntshoko <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 18:35:29 by kntshoko          #+#    #+#             */
-/*   Updated: 2019/05/24 18:36:32 by kntshoko         ###   ########.fr       */
+/*   Updated: 2019/05/27 10:21:37 by kntshoko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 {
 	char *dd = dst;
    	const char *ss = (const void*)src;
+	int len;
 
-
+	len = 0;
 	if (dd < ss)
 		while (n--)
 			*dd++ = *ss++;
 	else
 	{
 		while (n--)
-			(unsigned char)(dd+n) = (unsigned char)(ss+n);
+		dd[len+n] = ss[len+n];
 	}
 	return(dst);
 }
